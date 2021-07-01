5a6a7d6a343b58945aadadccfb99a5872ba180a6
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-07-01 15:03:37 -0300

    Update 2-NaiveBayes.py

--- 2-NaiveBayes.py
+++ 2-NaiveBayes.py
@@ -26,3 +26,27 @@
 #Entao se eu por em uma ordem nada ver, ele ainda vai considera naive
 #Em machine learning, a gente fala que ao ignorar o lingo entre a palavras ele tem um high bias
 #Mas ja que ele funfa ele tem uma variancia baixa
+#DISTRIBUICAO NORMAL
+#Antes de entender o que e uma gaussian naive bayes classifier
+#Temos que entender o que e uma distribuicao normal
+#Imagine um grafico de altura como eixo x, por numero de pessoas
+#O grafico vai ter um centro medio, onde vai ter o maior numero de pessoas. E ele provavlemente vai ser na altura media
+#Isso e chamado de uma distribuicao encurvada
+#A inclinacao dessa curva e decidida pela deviacao comum, quando tiver uma deviacao baixa
+#Ela vai aparentar ter um pino que ascende e cai rapidamente
+#Senao e uma mais gradual
+#Para desenhar uma distribuicao normal
+#Voce precisa saber de duas coisas, a media das medidas. Isso fala onde e o centro do pino
+#E a deviacao comun, que como dito acima define a inclinacao da curva
+#NAIVE BAYES GAUSSIANO
+#Uma curva gaussiana e a mesma coisa que uma distribuicao normal
+#Imagine 3 fatores, em relacao a uma pessoa que gosta ou nao de um filme
+#Tpo quanto pipoca ela come, quanto doces, e o quanto ela bebe de refrigerante
+#Ae a gente desenha, as distribuicoes gaussiana de cada uma dessas estatisticas de acordo com a divisao criada
+#Depois disso, a gente multiplica a prior probability (probabilidade inicial)* a likelihood (basicamente ponto no grafico no eixo y, que corresponde ao quanto ela come de pipoca Etc..)
+#A mesma coisa e feita com os outros, no entanto um deles teve um valor muitto baixo
+#Entao a gente pega o log desse valor, para evitar underflow
+#Underflow, ocorre quando o computador nao manten mais o controle do valor muito proximo de 0 ,para evitar erros a gente pega o log de tudo
+#Geralmente e o ln (log com base euler),
+#E o log transforma a multiplicacao para a soma do logs dos  valores log(prior probability)+log(likelihood de comer pipoca)+log(de bebe soda) e assim vai
+#Ae a gente calcula, akl que tiver o maior valor ganha
