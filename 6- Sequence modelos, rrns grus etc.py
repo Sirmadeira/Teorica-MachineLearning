537987acd426a9f51bbfc782571afe15ee3deceb
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-07-05 16:01:57 -0300

    Create 6- Sequence modelos, rrns grus etc.py

---
+++ 6- Sequence modelos, rrns grus etc.py
@@ -0,0 +1,5 @@
+#Recurrent neural network
+#RNNs sao redes neurais que sao boas em modelar sequence data
+#Imagine que voce tira  uma fota de uma bola movendo e voce quer adivinhar a direcao que ela vai somente com a imagen
+#Agora imagine que voce tirou um monte de fotos
+#O nome disso e data sequencial, ou sequencial
