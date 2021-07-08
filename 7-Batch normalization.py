93900b95647a1d8b94cd111d8efe9370b5f4e522
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-07-08 16:44:33 -0300

    Update 7-Batch normalization.py

--- 7-Batch normalization.py
+++ 7-Batch normalization.py
@@ -1,1 +1,6 @@
-#Normalization e quando voce tentar por a sua data no mesmo estilo
+#Normalization e quando voce executa a seguinte formula, z=x-m/s, m e a media do dataset, s e o standard deviation e o x e o data point
+#A primeira coisa que acontece numa batch normalization, e normalizar o output de uma funcao de ativacao,
+# ou seja executar a funcao acima sendo x o ponto de interesee
+#Depois ele multiplica o output por um parametro arbitrario z*g
+#E por fim, ele soma outra parametro arbitrario(x*g)+b
+#Ele aumenta a velocidade de analise de dados, e alem do mais corrige pesos outliers
