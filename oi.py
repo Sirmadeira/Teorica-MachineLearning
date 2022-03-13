56616e47d63b73fd3a7ffb5a91426b8bf4ca4376
Yappy <francisco.froes@globalhitss.com.br>
2022-03-13 13:02:22 -0300

    Create oi.py

---
+++ oi.py
@@ -0,0 +1,17 @@
+
+lista = [[1,2,3,4,5],[9,2,3,14,5]]
+
+# for i in range(len(lista)):
+# 	if i%2:
+# 		print(f'index impares{lista[i]}')
+# 	else:
+# 		print(f'Index pares{lista[i]}')
+
+
+
+for i in range(len(lista)):
+	print(i)
+	for j in lista[i]:
+		lista[i]
+		# if j %2 == 0:
+		# 	lista[i][].apppend(j * 2)
